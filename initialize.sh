1a0af4c5708090cc84f0b18895e2a408c5aacf8f
Prathamesh Joshi <prathamesh1729@gmail.com>
2020-05-23 12:53:15 +0530

    build(app.json): Fix postdeploy script

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Reference: createsuperuser: https://docs.djangoproject.com/en/3.0/ref/django-admin/#createsuperuser
+# Reference: multiple scripts in postdeploy: https://stackoverflow.com/a/46221729/13193202
 
 python manage.py migrate
 python manage.py createsuperuser --no-input
